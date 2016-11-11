d8d446a0cfdeee43b333fc957b2fd50206772436
wangdk <123456a?>
2016-11-11 14:50:16 +0800

    bug:mount lv to another namespace

--- driver.go
+++ driver.go
@@ -6,11 +6,14 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 
-	_ "github.com/Unknwon/goconfig"
+	"github.com/op/go-logging"
+
 	"github.com/docker/go-plugins-helpers/volume"
 )
 
@@ -22,29 +25,46 @@ const (
 )
 
 type LvmPersistDriver struct {
-	Volumes map[string]string //key:volume name,value:volume device name
-	Mutex   *sync.Mutex
-	Name    string
-	Mounts  map[string]string // key:volume name, value: mountpoint ids
-	VgName  string
-	//MountCounts map[string]int64
+	Volumes     map[string]string //key:volume name,value:volume device name
+	Mutex       *sync.Mutex
+	Name        string
+	Mounts      map[string]string // key:volume name, value: mountpoint ids
+	VgName      string
+	MountCounts map[string]int
 }
 
+var log = logging.MustGetLogger("example")
+
+var format = logging.MustStringFormatter(
+	`%{time:15:04:05.000} %{shortfunc} %{level:.4s}:%{message}`,
+)
+
 func NewLvmPersistDriver() *LvmPersistDriver {
-	fmt.Println("Starting... ")
-	os.Mkdir(PluginDataDir, 0700)
-	os.Mkdir(LvmVolumeDir, 0700)
+
+	logFile, err := os.OpenFile("/var/log/docker-volume-plugins.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	backend2 := logging.NewLogBackend(logFile, "", 0)
+
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+	logging.SetBackend(backend2Formatter)
+	logging.SetLevel(logging.INFO, "")
+
+	log.Info("Starting... ")
+	os.Mkdir(PluginDataDir, 0750)
+	os.Mkdir(LvmVolumeDir, 0750)
 	driver := initialCache()
 
-	fmt.Printf("Found %s volumes on startup\n", strconv.Itoa(len(driver.Volumes)))
+	log.Info("Found %s volumes on startup\n", strconv.Itoa(len(driver.Volumes)))
 	return &driver
 }
 
 func (driver *LvmPersistDriver) Get(req volume.Request) volume.Response {
-	fmt.Println("list volume ")
+	log.Info("list volume ")
 
 	if driver.exists(req.Name) {
-		fmt.Println("Found %s\n", req.Name)
+		log.Error("Found %s\n", req.Name)
 		return volume.Response{
 			Volume: driver.volume(req.Name),
 		}
@@ -55,14 +75,14 @@ func (driver *LvmPersistDriver) Get(req volume.Request) volume.Response {
 }
 
 func (driver *LvmPersistDriver) List(req volume.Request) volume.Response {
-	fmt.Println("List Called... ")
+	log.Info("List Called... ")
 
 	var volumes []*volume.Volume
 	for name, _ := range driver.Volumes {
 		volumes = append(volumes, driver.volume(name))
 	}
 
-	fmt.Printf("Found %s volumes\n", strconv.Itoa(len(volumes)))
+	log.Info("Found %s volumes\n", strconv.Itoa(len(volumes)))
 
 	return volume.Response{
 		Volumes: volumes,
@@ -76,15 +96,15 @@ func (driver *LvmPersistDriver) Create(req volume.Request) volume.Response {
 	defer func() {
 		driver.Mutex.Unlock()
 		if r := recover(); r != nil {
-			//TODO clean garbage data
-			//return volume.Response{Err: "create volume failed"}
+			debug.PrintStack()
+			//			return volume.Response{Err: "create volume failed"}
 
 		}
 	}()
-	fmt.Print("Create Called... ")
+	log.Info("Create Volume")
 	volumeSize, ok := req.Options["size"]
 	if !ok || (ok && volumeSize == "") {
-		fmt.Sprintf("The volume %s size is zero,use default 2G", req.Name)
+		log.Info("The volume %s size is zero,use default 2G", req.Name)
 		volumeSize = "2G"
 	}
 	if driver.exists(req.Name) {
@@ -96,20 +116,18 @@ func (driver *LvmPersistDriver) Create(req volume.Request) volume.Response {
 	//1. create lv:lvcreate -L $lvsize -n $lvname $vgname -y
 	vgName, vgOk := req.Options["vg"]
 	if !vgOk || (vgOk && vgName == "") {
-		fmt.Sprintf("The no vg info in req use default vg")
+		log.Info("There is no vg info in req use default vg")
 		vgName = driver.VgName
 	}
 
 	cmdArgs := []string{"-n", req.Name}
 	cmdArgs = append(cmdArgs, "-L", volumeSize)
 	cmdArgs = append(cmdArgs, vgName)
-	fmt.Println(cmdArgs)
 	cmd := exec.Command("lvcreate", cmdArgs...)
 	_, err := cmd.CombinedOutput()
-
 	if err != nil {
-		fmt.Print("create lv from vg error", err)
-		panic("create lv from vg error")
+		log.Error("create lv from vg error", err)
+		return volume.Response{Err: "create lv failed"}
 	}
 
 	lvdiskname := fmt.Sprintf("/dev/%s/%s", vgName, req.Name)
@@ -117,39 +135,43 @@ func (driver *LvmPersistDriver) Create(req volume.Request) volume.Response {
 	cmd = exec.Command("mkfs.xfs", lvdiskname)
 	_, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("format lv failed", err)
-		return volume.Response{Err: fmt.Sprintf("format volume %s failed", req.Name)}
+		log.Error("format lv failed", err)
+		return volume.Response{Err: "format lv failed"}
 	}
 	//3.persist mount lv to mountPoint
 	mountPoint := LvmVolumeDir + req.Name
-
-	os.Mkdir(mountPoint, 0644)
+	err = syscall.Mkdir(mountPoint, 0750)
+	if err != nil {
+		log.Error("mkdir mountpoint failed", err)
+		return volume.Response{Err: "mkdir mountpoint failedss"}
+	}
 	cmdArgs = []string{lvdiskname, mountPoint}
 	fmt.Println(cmdArgs)
 	cmd = exec.Command("mount", cmdArgs...)
 	if _, err = cmd.CombinedOutput(); err != nil {
-		fmt.Println("mount lv failed", err)
+		log.Error("mount lv failed", err)
 		return volume.Response{Err: fmt.Sprintf("volum mount failed")}
-	} else {
-		//update /etc/fstab
-		cmdArgs = []string{"/etc/fstab", "/etc/fstab.back"}
-		cmd = exec.Command("cp", cmdArgs...)
-		if _, err1 := cmd.CombinedOutput(); err1 != nil {
-			fmt.Println("back fstab failed", err1)
-		}
-		content := lvdiskname + " " + mountPoint + " xfs defaults 0 1"
-		updateFstab(content, false)
 	}
+
+	//bug:syscall mount to another namspace
+	//	err = syscall.Mount(lvdiskname, mountPoint, "xfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "")
+
+	//	if err != nil {
+	//		log.Error("mount lv failed", err)
+	//		return volume.Response{Err: "mount lv failed"}
+	//	}
+	content := lvdiskname + " " + mountPoint + " xfs defaults 0 1"
+	updateFstab(content, false)
 
 	//3 persist to data dir
-	cmd = exec.Command("touch", PluginDataDir+req.Name)
-	_, err = cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println("persist voulme info failed", err)
-		return volume.Response{Err: fmt.Sprintf("internal error")}
-	}
 
-	fmt.Println("disk name is %s", lvdiskname)
+	//	cmd = exec.Command("touch", PluginDataDir+req.Name)
+	//	_, err = cmd.CombinedOutput()
+	//	if err != nil {
+	//		fmt.Println("persist voulme info failed", err)
+	//		return volume.Response{Err: fmt.Sprintf("internal error")}
+	//	}
+	//update cache info
 	driver.Volumes[req.Name] = lvdiskname
 	driver.Mounts[req.Name] = mountPoint
 	driver.UpdateCacheFile()
@@ -157,89 +179,80 @@ func (driver *LvmPersistDriver) Create(req volume.Request) volume.Response {
 }
 
 func (driver *LvmPersistDriver) Remove(req volume.Request) volume.Response {
-	fmt.Println("Remove Called... ")
+	log.Info("Remove Volume ")
 	driver.Mutex.Lock()
 	defer func() {
 		driver.Mutex.Unlock()
 		if r := recover(); r != nil {
-			fmt.Println("Remove volume err ", r)
-			//
+			debug.PrintStack()
+			log.Error("Remove volume err ", r)
+			//return volume.Response{Err: "remove volume failed"}
 		}
 	}()
 	deviceName := driver.Volumes[req.Name]
-	//0 check any mount info
-	//	if _, ok := driver.Mounts[req.Name]; ok {
-	//		return volume.Response{Err: fmt.Sprintf("this volume has mount point ,can not remove")}
-	//	}
-
-	//umount
-	cmd := exec.Command("umount", deviceName)
-	if _, err := cmd.CombinedOutput(); err != nil {
-		fmt.Println("umount lv failed", err)
-		return volume.Response{Err: fmt.Sprintf("remove volume %s failed", req.Name)}
+	//0 check
+	if driver.MountCounts[req.Name] > 0 {
+		log.Error("volume is mounted,can not remove ")
+		return volume.Response{Err: "remove volume failed"}
 	}
-
-	//update fstab
+	//1 umount
+	err := syscall.Unmount(driver.Mounts[req.Name], 0)
+	if err != nil {
+		log.Error("unmount lv failed", err)
+		return volume.Response{Err: "unmount lv failed"}
+	}
+	//2 update fstab
 	content := deviceName + " " + driver.Mounts[req.Name] + " xfs defaults 0 1"
 	updateFstab(content, true)
-	//2. remove from vg  $lvdiskname -f
+	//3. remove from vg  $lvdiskname -f
 	cmdArgs := []string{deviceName, "-f"}
-	cmd = exec.Command("lvremove", cmdArgs...)
+	cmd := exec.Command("lvremove", cmdArgs...)
 	if _, err := cmd.CombinedOutput(); err != nil {
-		fmt.Println("remove voulme info failed", err)
-		return volume.Response{Err: "remove volume failed"}
+		log.Error("remove lv  failed", err)
+		return volume.Response{Err: "remove lv failed"}
 	}
-	//remove locl dir
-	os.RemoveAll(driver.Mounts[req.Name])
-	//3. remove from persist data
-	cmd = exec.Command("rm", PluginDataDir+req.Name)
-	_, err := cmd.CombinedOutput()
+	//remove local dir
+	err = os.RemoveAll(driver.Mounts[req.Name])
 	if err != nil {
-		fmt.Print("remove voulme info failed", err)
-		return volume.Response{Err: "remove volume failed"}
+		log.Error("remove voulme info failed", err)
+		return volume.Response{Err: "remove local dir failed"}
 	}
 	//1.remove from cache
 	delete(driver.Volumes, req.Name)
 	delete(driver.Mounts, req.Name)
-
 	driver.UpdateCacheFile()
-
 	return volume.Response{}
 }
 
 func (driver *LvmPersistDriver) Mount(req volume.Request) volume.Response {
 	driver.Mutex.Lock()
 	defer driver.Mutex.Unlock()
-	fmt.Println("Mount Called... ")
-	devicename := driver.Volumes[req.Name]
-	if devicename == "" {
+	log.Info("Mount Called... ")
+	mountPoint := driver.Mounts[req.Name]
+	if mountPoint == "" {
 		return volume.Response{Err: fmt.Sprintf("The volume %s not exist", req.Name)}
 	}
-	fmt.Println(devicename)
-	mountPoint := LvmVolumeDir + req.Name
-
-	driver.UpdateCacheFile()
+	driver.MountCounts[req.Name] = driver.MountCounts[req.Name] + 1
 	return volume.Response{Mountpoint: mountPoint}
 }
 
 func (driver *LvmPersistDriver) Path(req volume.Request) volume.Response {
-	devicename := driver.Volumes[req.Name]
-
-	if devicename != "" {
+	mountPoint := driver.Mounts[req.Name]
+	if mountPoint == "" {
 		return volume.Response{Err: fmt.Sprintf("The volume %s not exist", req.Name)}
 	}
-	return volume.Response{Mountpoint: LvmVolumeDir + req.Name}
+	return volume.Response{Mountpoint: mountPoint}
 }
 
 func (driver *LvmPersistDriver) Unmount(req volume.Request) volume.Response {
 	driver.Mutex.Lock()
 	defer driver.Mutex.Unlock()
-	fmt.Println("Unmount Called... ")
+	log.Info("Unmount Called... ")
 	_, ok := driver.Volumes[req.Name]
 	if !ok {
 		return volume.Response{Err: fmt.Sprintf("The volume %s not exist", req.Name)}
 	}
-
+	driver.MountCounts[req.Name] = driver.MountCounts[req.Name] - 1
 	driver.UpdateCacheFile()
 	return volume.Response{}
 }
@@ -262,6 +275,7 @@ func initialCache() LvmPersistDriver {
 		json.Unmarshal(bytes, &data)
 		driver.Volumes = data.Volumes
 		driver.Mounts = data.Mounts
+		driver.MountCounts = data.MountCounts
 	}
 	if driver.Mounts == nil {
 		driver.Mounts = make(map[string]string)
@@ -269,22 +283,24 @@ func initialCache() LvmPersistDriver {
 	if driver.Volumes == nil {
 		driver.Volumes = make(map[string]string)
 	}
-
+	if driver.MountCounts == nil {
+		driver.MountCounts = make(map[string]int)
+	}
 	driver.VgName = "VG0"
 
 	return driver
 }
 
 func (driver *LvmPersistDriver) UpdateCacheFile() {
-	fmt.Println("UpdateCacheFile")
+	log.Info("UpdateCacheFile")
 	data, err := json.Marshal(driver)
 	if err != nil {
-		fmt.Println(err)
+		log.Error(err)
 	}
 	//	fmt.Println("cache data is %s", string(data))
-	err = ioutil.WriteFile(DriverCacheFile, data, 0644)
+	err = ioutil.WriteFile(DriverCacheFile, data, 0755)
 	if err != nil {
-		fmt.Println("update cache filed failed", err)
+		log.Error("update cache filed failed", err)
 	}
 }
 
@@ -299,28 +315,37 @@ func (driver *LvmPersistDriver) exists(name string) bool {
 
 ////append or delete lv info in /etc/fstab
 func updateFstab(content string, isDelete bool) {
+
+	//backup /etc/fstab
+	cmdArgs := []string{"/etc/fstab", "/etc/fstab.back"}
+	cmd := exec.Command("cp", cmdArgs...)
+	if _, err1 := cmd.CombinedOutput(); err1 != nil {
+		log.Error("back fstab failed", err1)
+	}
+
 	if isDelete {
 		removeLineFrom(content)
 		return
 	}
+
 	f, err := os.OpenFile("/etc/fstab", os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Println("open fstab failed", err)
+		log.Error("open fstab failed", err)
 		return
 	}
 	defer f.Close()
 
 	_, err = f.WriteString(content + "\n")
 	if err != nil {
-		fmt.Println("update fstab failed", err)
+		log.Error("update fstab failed", err)
 	}
 }
 
 func removeLineFrom(line string) {
 	input, err := ioutil.ReadFile("/etc/fstab")
-	fmt.Println("delte line is ", line)
+	log.Info("delte line is ", line)
 	if err != nil {
-		fmt.Println("read fstab failed", err)
+		log.Error("read fstab failed", err)
 		return
 	}
 
@@ -341,6 +366,6 @@ func removeLineFrom(line string) {
 	output := strings.Join(lines, "\n")
 	err = ioutil.WriteFile("/etc/fstab", []byte(output), 0644)
 	if err != nil {
-		fmt.Println("read fstab failed", err)
+		log.Error("read fstab failed", err)
 	}
 }
